2dbcaa1ca9647cff3d490e39c1f894c021d10543
Rik Maas <rmaas@anago.nl>
2025-10-03 12:15:03 +0200

    Use highest rated player again, but do not cache it

--- AnagoLeaderboard/AnagoLeaderboard/Services/PlayerService.cs
+++ AnagoLeaderboard/AnagoLeaderboard/Services/PlayerService.cs
@@ -134,8 +134,7 @@ namespace AnagoLeaderboard.Services
         {
             var players = await GetPlayers();
             var highestRatedPlayer = players.OrderByDescending(player => player.Rating).FirstOrDefault();
-            var randomPlayer = players[new Random().Next(players.Count)];
-            var filePath = @$"C:\tafelvoetbal\tafelvoetbal-server\data\avatars\{randomPlayer.Id}";
+            var filePath = @$"C:\tafelvoetbal\tafelvoetbal-server\data\avatars\{highestRatedPlayer.Id}";
             if (!File.Exists(filePath))
             {
                 filePath = @$"C:\tafelvoetbal\tafelvoetbal-server\data\avatars\empty-avatar.jpg";
